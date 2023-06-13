17990abbac44de1ddf5f927c4706d91049a452e2
alexgagnon <alex@monad.media>
2023-06-13 12:09:13 +0800

    fix replace

--- packages/css/packages/css/README.txt
+++ packages/css/packages/css/README.txt
@@ -4,16 +4,19 @@ The css files that make up Declare.css.
 
 Exceptions:
 - font-style has acronym `fi`
-- display and position names are just the values (i.e. `relative` instead of `p-r`), which frees up `p` for padding
+- position names are just the values (i.e. `relative` instead of `p-r`), which frees up `p` for padding
+- min and max widths and heights are minw/maxw/minh/maxh so they don't collide with `m` for margin
+- basic text styling has shortcuts `i` and `b` for italic and bold (but prefer `<strong>` and `<emphasis>` tags for semantics, when appropriate)
 
 ## Common confusions:
 
-- .f is for flex, all other properties that start with f are for fonts, i.e. .ff, .fs
+- .f is for flex, and .fd is for flex-direction, all other properties that start with f are for fonts, i.e. .ff, .fs
 
 ## TODO
 
+- [ ] generate index.css files depending on inclusions and bundling
 - [ ] postcss-replace from a config file
 - [ ] optional typography override?
 - [ ] optional complement theme?
 - [ ] optional components?
-- [ ] eject option
+- [ ] eject option?
--- packages/css/packages/css/lib/index.js
+++ packages/css/packages/css/lib/index.js
@@ -1,31 +1,67 @@
-const { readFileSync } = require("fs");
+#!/usr/bin/env node
 
-// TODO: check schema
-function normalizeConfig(configPath = '../default.config.js') {
-  const config = module.require(configPath);
-  const normalizedConfig = flattenObject(config);
-  // console.debug(config, normalizedConfig);
-  return normalizedConfig;
-}
+const {join, sep} = require("path");
+const {writeFile, stat} = require("fs").promises;
+const {mkdirp, sort_keys} = require('./utils');
+const {green} = require('kleur/colors');
+const currentPackage = require('../package.json');
 
-function flattenObject(input) {
-  const result = {};
-  for (const key in input) {
-    if (!input.hasOwnProperty(key)) {
-      continue;
-    }
-    if (typeof input[key] === "object" && !Array.isArray(input[key])) {
-      var subFlatObject = flattenObject(input[key]);
-      for (const subkey in subFlatObject) {
-        result[key + "_" + subkey] = subFlatObject[subkey];
-      }
-    } else {
-      result[key] = input[key];
-    }
+let DEFAULT_FILE = join(process.cwd(), "declare.config.js");
+let DEFAULT_SCRIPTNAME = "declare";
+let DEFAULT_FORCE = false;
+
+async function init(
+  filename,
+  force = DEFAULT_FORCE,
+  scriptname = DEFAULT_SCRIPTNAME
+) {
+  const to = filename != null ? join(process.cwd(), filename) : DEFAULT_FILE;
+  try {
+    await stat(to);
+    if (!force)
+      throw new Error(
+        "Output file already exists, use -F/--force if you really want to override"
+      );
+  } catch (error) {
+    process.exit(1);
   }
-  return result;
+
+  // copy default theme file, updating reference to JSON schema file
+  await mkdirp(to.split(sep).slice(0, -1).join(sep));
+  const themeFile = require("../default.config.js");
+
+  // TODO: publish to schema repo
+  // themeFile.$schema = join(
+  //   process.cwd(),
+  //   "node_modules",
+  //   "declare.css",
+  //   "schema.json"
+  // );
+
+  await writeFile(to, themeFile, {encoding: "utf8"});
+
+  console.log(green(`Theme file copied to ${to}`));
+
+  // update package file to include new scripts and dependency
+  const packagePath = join(process.cwd(), "package.json");
+  try {
+    await stat(packagePath);
+    const packageFile = require(packagePath);
+    packageFile.scripts[scriptname] = `npm run ${scriptname}:build -- --watch`;
+    packageFile.scripts[`${scriptname}:build`] = `declare build ${to}`;
+
+    packageFile.devDependencies["declare"] = currentPackage.version;
+    await writeFile(packagePath, JSON.stringify(packageFile, null, 2), {
+      encoding: "utf8",
+    });
+    console.log(
+      `Success! \`npm install\` and \`npm run ${scriptname}\` to get started`
+    );
+  } catch (error) {
+    throw new Error("Could not update the package file");
+  }
 }
 
-module.exports = {
-  normalizeConfig
-};
+(async () => {
+  await init();
+})();
