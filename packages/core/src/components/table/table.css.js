a4be13b728c5d2c947d835d1654b4ca92134ac01
alexgagnon <alexgagnon@users.noreply.github.com>
2024-02-28 12:53:53 -0500

    Create table.css.js

---
+++ packages/core/src/components/table/table.css.js
@@ -0,0 +1,55 @@
+import { css } from 'lit';
+
+export default css`
+table {
+  border-collapse: collapse;
+}
+
+td {
+  padding: var(--fui-spacing-base);
+}
+
+thead {
+  border-bottom: 1px solid black;
+}
+
+th {
+  text-align: start;
+  display: table-cell;
+}
+
+th > button {
+  text-align: start;
+  padding: var(--fui-spacing-responsive);
+  display: flex;
+  flex-direction: row;
+  align-items: top;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 1rem;
+  font-family: var(--fui-font-family-body);
+}
+
+th .sort-icons::before {
+  content: '▲';
+}
+
+th .sort-icons::after {
+  content: '▼';
+}
+
+tfoot {
+  border-top: 1px solid black;
+}
+tfoot th {
+  padding: var(--fui-spacing-base);
+}
+
+button {
+  background-color: unset;
+}
+
+[aria-pressed=true] {
+  background: var(--fui-color-grey);
+}
+`;
